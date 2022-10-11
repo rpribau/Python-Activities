bfd5557af757a135c806420814bf56fd2d9cbbd3
Rob <106797059+rpribau@users.noreply.github.com>
2022-10-11 11:25:28 -0500

    New test

--- test2.py
+++ test2.py
@@ -1,2 +1,8 @@
-a = 3%2
-print(a)
+cadena = input("Escribe un texto: ")
+letra = input("Escribe la letra que deseas conservar: ")
+def reemplazar(texto, letra):
+    for i in texto:
+        if i != letra:
+            texto = texto.replace(i,'*')
+    return texto
+print(reemplazar(cadena,letra))
